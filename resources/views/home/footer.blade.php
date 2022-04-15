f0292ba87dcaa67a33232a87eef486bc1a45096b
dilarabehar <dilarabehar71@gmail.com>
2022-04-15 18:59:22 +0300

    Template Implementation

---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,23 @@
+
+    <!-- footer section -->
+    <footer class="footer_section">
+        <div class="container">
+            <p>
+                &copy; <span id="displayYear"></span> All Rights Reserved By
+                <a href="https://html.design/">Free Html Templates</a><br>
+                Distributed By: <a href="https://themewagon.com/">ThemeWagon</a>
+            </p>
+        </div>
+    </footer>
+    <!-- footer section -->
+</div>
+<!-- jQery -->
+<script src="{{asset('assets')}}/js/jquery-3.4.1.min.js"></script>
+<!-- bootstrap js -->
+<script src="{{asset('assets')}}/js/bootstrap.js"></script>
+<!-- slick  slider -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.js"></script>
+<!-- nice select -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js" integrity="sha256-Zr3vByTlMGQhvMfgkQ5BtWRSKBGa2QlspKYJnkjZTmo=" crossorigin="anonymous"></script>
+<!-- custom js -->
+<script src="{{asset('assets')}}/js/custom.js"></script>
--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -1,18 +1,53 @@
+<!DOCTYPE html>
 <html>
+
 <head>
-    <title>App Name - @yield('title')</title>
-    @yield('head')
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>@yield("title")</title>
+
+
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/css/bootstrap.css"/>
+
+    <!-- fonts style -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet">
+
+    <!-- font awesome style -->
+    <link href="{{asset('assets')}}/css/font-awesome.min.css" rel="stylesheet" />
+    <!-- nice select -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css" integrity="sha256-mLBIhmBvigTFWPSCtvdu6a76T+3Xyt+K571hupeFLg4=" crossorigin="anonymous" />
+    <!-- slidck slider -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css" integrity="sha256-UK1EiopXIL+KVhfbFa8xrmAWPeBjMVdvYMYkTAEv/HI=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css.map" integrity="undefined" crossorigin="anonymous" />
+
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('assets')}}/css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="{{asset('assets')}}/css/responsive.css" rel="stylesheet" />
+    @yield("head")
 </head>
+
 <body>
-<h1>Header</h1>
-@section('sidebar')
-    This is the master sidebar.
+
+@include("home.header")
+@section('slider')
+
 @show
 
-<div class="container">
+
     @yield('content')
-</div>
-<h1>Footer</h1>
+
+@include("home.footer")
 @yield('foot')
 </body>
 </html>
